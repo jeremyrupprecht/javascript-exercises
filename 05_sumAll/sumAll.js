173e22be88c90d272bb0bc07e1a0a8c07ea19c66
jeremyrupprecht <jdavid.rupprecht@gmail.com>
2023-09-13 17:14:37 -0600

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+/*
+    This function takes two numbers, and returns the sum of every number
+    between(and including) them
+*/
 
+const sumAll = function(num1, num2) {
+
+    // validate for negative and non-number parameters
+    if (typeof num1 != 'number' ||
+        typeof num2 != 'number' ||
+        num1 < 0 || 
+        num2 < 0) {
+            return "ERROR";
+     }
+    // num1 should hold the smaller number
+    // for the loop to work
+    if (num1 > num2) {
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    let sum = 0;
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+        
+    }
+    return sum;
 };
 
 // Do not edit below this line
