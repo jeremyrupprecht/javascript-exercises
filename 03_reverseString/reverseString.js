ce4c3d430daeeee0cb0d46e0ca266f353a608fed
jeremyrupprecht <jdavid.rupprecht@gmail.com>
2023-09-13 15:22:53 -0600

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,26 @@
-const reverseString = function() {
+/* This function reverses a string, this is done by repeatedly taking the last
+   character of the initial string and placing it at the end/tail of a new empty
+   string. This is done for every character in the initial string.
+   
+   example:     initial string      reversed string
 
+                "hello"      ->      ""
+                "hell"       ->      "o"
+                "hel"        ->      "ol"
+                "he"         ->      "oll"
+                "h"          ->      "olle"
+                ""           ->      "olleh"
+*/
+const reverseString = function(stringToReverse) {
+
+    let initialCharacterArray = stringToReverse.split("");
+    let reversedCharacterArray = [];
+
+    while (initialCharacterArray.length > 0) {
+        let lastChar = initialCharacterArray.pop();
+        reversedCharacterArray.push(lastChar);
+    }   
+    return reversedCharacterArray.join("");
 };
 
 // Do not edit below this line
