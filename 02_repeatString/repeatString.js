99085d62b7255ac792fed75355ecb7193542a858
jeremyrupprecht <jdavid.rupprecht@gmail.com>
2023-09-13 14:44:01 -0600

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, repeats) {
+
+    if (repeats < 0) {
+        return "ERROR";
+    }
+
+    repeatedString = "";
+    for (let i = 0; i < repeats; i++) {
+        repeatedString += stringToRepeat;
+    }
+    return repeatedString;
 
 };
 
