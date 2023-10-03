5435e99ba38daf8bcfa8fd08cb566beaa44eca7f
Jeremy <jdavid.rupprecht@gmail.com>
2023-10-02 22:27:10 -0600

    Complete exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(inputArr) {
+	return inputArr.reduce((sum, number) => sum + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(inputArr) {
+	return inputArr.reduce((sum, number) => sum * number, 1);
 };
 
-const power = function() {
-	
+const power = function(a ,b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  // interative version
+/*   if (n == 0) {
+    return 1
+  }
+  let sum = 1;
+  while (n > 0) {
+    sum *= n;
+    n -= 1;
+  }
+  return sum */
+
+  // recursive version
+  if (n == 0) {
+    return 1
+  }
+  return n * factorial(n-1)
 };
+
+
 
 // Do not edit below this line
 module.exports = {
