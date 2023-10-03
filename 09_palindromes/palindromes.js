61581f3a71803908ef31f5a63458d7be2ff60253
Jeremy <jdavid.rupprecht@gmail.com>
2023-10-02 22:28:14 -0600

    Complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[\.,?!\s]/g, "")
+    // Use the Two Pointers algorithm
+    let l = 0
+    let r = str.length-1
+    while (l < r) {
+        if (str[l] !== str[r]) {
+            return false
+        }
+        l += 1
+        r -= 1
+    }
+    return true
 };
 
 // Do not edit below this line
