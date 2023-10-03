c2559956312c647b82178c2be74520805f01269f
Jeremy <jdavid.rupprecht@gmail.com>
2023-10-02 23:50:15 -0600

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+function checkIfPersonStillAlive(person) {
+    if (!person.hasOwnProperty("yearOfDeath")) {
+        person["yearOfDeath"] = new Date().getFullYear();
+    }
+}
 
-};
+const findTheOldest = (people) => people.reduce((oldestPerson, person) => {
+    checkIfPersonStillAlive(person);
+    checkIfPersonStillAlive(oldestPerson);
+    if ((person.yearOfDeath - person.yearOfBirth) > 
+        (oldestPerson.yearOfDeath - oldestPerson.yearOfBirth)) {
+        return person // becomes the new oldestPerson
+    }
+    return oldestPerson // otherwise keep the current oldestPerson
+}, people[0]);
+
+// (person.yearOfDeath - person.yearOfBirth)
 
 // Do not edit below this line
 module.exports = findTheOldest;
