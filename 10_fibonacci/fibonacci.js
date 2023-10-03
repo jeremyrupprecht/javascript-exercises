a2a554640f674d97c43af085226d224d3de451e2
Jeremy <jdavid.rupprecht@gmail.com>
2023-10-02 23:01:30 -0600

    Complete exercise 10_fibonacci - Iterative solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS"
+    }
+    if (typeof(n) === "string") {
+        n = +n;
+    }
+    // Iterative solution
 
+    // 1, 1, 2, 3,
+    let prev1 = 1;
+    let prev2 = 0;
+    let sum = prev1 + prev2;
+    for (let i = 1; i < n; i++) {
+        sum = prev1 + prev2;
+        prev2 = prev1
+        prev1 = sum
+    }
+    return sum
+
+    // Recursive solution
 };
 
 // Do not edit below this line
